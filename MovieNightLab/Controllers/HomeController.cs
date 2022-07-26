30ffa712007ba732bb1b38d578370dd7223bd5a4
Connor <Woodconnor13@gmail.com>
2022-07-26 16:44:06 -0400

    First commit for movie API lab

---
+++ MovieNightLab/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieNightLab.Models;
+using System.Diagnostics;
+
+namespace MovieNightLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public MovieDAL movieDAL = new MovieDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]//run when you first go to page
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+        [HttpPost]//run when you submit your form
+        public IActionResult MovieSearch(string title)
+        {
+            MovieModel movie = movieDAL.GetMovie(title);
+            return View(movie);
+        }
+
+        [HttpGet]//run when you first go to page
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+        [HttpPost]//run when you submit your form
+        public IActionResult MovieNight(string title1, string title2, string title3)
+        {
+            List<MovieModel> movies = new List<MovieModel>();
+            movies.Add(movieDAL.GetMovie(title1));
+            movies.Add(movieDAL.GetMovie(title2));
+            movies.Add(movieDAL.GetMovie(title3));
+            return View(movies);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieNightLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieNightLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
